28304ad9d028ba2c26a76496de623a4fcadab848
jrequarth <jarequar@ncsu.edu>
2023-12-08 19:46:01 -0500

    added quantity and check to see if integer and added while loop to see if the customer wants to order another item

--- menu.py
+++ menu.py
@@ -1,5 +1,5 @@
 # Menu dictionary
-menu = {
+menu-items = {
     "Snacks": {
         "Cookie": .99,
         "Banana": .69,
@@ -52,8 +52,66 @@ menu = {
 
 menu_dashes = "-" * 42
 
+#create an empty order list with the following dictionary format
+# = [{"Item name": "string", "Price": float, "Quantity: int"}, {next item}]
+customers_order_list = []
+
+while place_order:
+    # menu_selection by customer
+    
+    #check to see if it is a number - if not print error and re-ask
+    
+    #change to integer
+    
+    #check to see if the integer is on the menu list - if not - error
+    
+    #use number to get a name from menu_items dictionary
+    item_name = ????
+    
+    #Ask customer for quantity of the item
+    quantity = input(f'How many {item_name}s do you want?')
+    
+    #check to see if quantity is an integer - if not quantity = 1
+    if type(quantity) is not int:
+        quantity = 1
+        print("Since you did not type an integer, quantity was set to 1.")
+        
+    
+    #append order list to have item_name, price, and quantity
+    
+    #ask customer if they want to order again with match/case
+    while True:
+        keep_ordering = input("Do you want to order another item?\ny=yes n=no")
+        #check customer's input
+        match keep_ordering.lower():
+            #customer chose yes
+            case 'y':
+                place_order = True
+                break
+            #customer chose no, so complete order and exit to print receipt
+            case 'n':
+                place_order = False
+                print("Thank you for your order.")
+                break
+            #customer gave an invalid response
+            case_:
+                print("I didn't understand your response. Please try again.")
+                print("Signed\nHAL-9000")
+    match keep_ordering.lower():
+        case 'y':
+            keep_ordering = True
+        case 'n':
+            keep_ordering = False
+            
+    
+
+    
+    
+
+
+
 # Launch the store and present a greeting to the customer
-print("Welcome to the variety food truck.")
+print("Welcome to the Order-By-Number food truck.")
 
 # Customers may want to view different sections of the menu, so let's create a 
 # continuous loop
